963a69fd22ef175823b591dd7853cf98e7c55c9d
Evgeny Berezhnov <33531079+JackBerezhnov@users.noreply.github.com>
2024-04-29 22:43:22 -0700

    Change colors of calendar to white

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Calendar from "./components/calendar/Calendar";
 
 export default function Home() {
   return (
-    <div className="flex items-center h-screen">
+    <div className="flex items-center h-screen bg-slate-900	">
       <Calendar />
     </div>
   );
