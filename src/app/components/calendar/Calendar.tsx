f7b6c26caa0d426f6e43d8ff07c73e744ee11ea5
Evgeny Berezhnov <33531079+JackBerezhnov@users.noreply.github.com>
2024-04-29 22:29:39 -0700

    Added Calendar Component

---
+++ src/app/components/calendar/Calendar.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
+import "./Calendar.css";
+
+const Calendar = () => {
+    return (
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DateCalendar />
+        </LocalizationProvider>
+    );
+}
+export default Calendar;
